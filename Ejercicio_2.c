f11183105461904315b8deaf803fda1bbc88eacd
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-17 01:39:21 -0500

    Inicializacion variables 2.0

--- Ejercicio_2.c
+++ Ejercicio_2.c
@@ -1,7 +1,8 @@
-//4. Leer dos números y mostrar todos los enteros comprendidos entre ellos.
-//Trejos Buriticá, O. I. (2017). Lógica de programación. Bogotá, Colombia: Ediciones de la U. Recuperado de https://elibro.net/en/ereader/udla/70315?page=227.
+//13. Leer un entero y mostrar todos los múltiplos de 5 comprendidos entre 1 y el número leído.
+//Trejos Buriticá, O. I. (2017). Lógica de programación. Bogotá, Colombia: Ediciones de la U. Recuperado de https://elibro.net/en/ereader/udla/70315?page=228.
 #include <stdio.h>
 
 int main(void) {
 //Inicializacion de las variables
-  int a, b, i;
+  int numero;
+//Damos un mensaje inicial para dar un valor a nuestra variable
