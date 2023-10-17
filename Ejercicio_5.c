66ad3f132d713aaedb4a39f209179730dd549a68
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-17 01:56:40 -0500

    Inicializacion de variables

---
+++ Ejercicio_5.c
@@ -0,0 +1,6 @@
+//40. Leer un número de dos dígitos y determinar si pertenece a la serie de Fibonacci.
+//Trejos Buriticá, O. I. (2017). Lógica de programación. Bogotá, Colombia: Ediciones de la U. Recuperado de https://elibro.net/en/ereader/udla/70315?page=230.
+#include <stdio.h>
+//Inicializacion de variables 
+int main() {
+    int numero, a = 0, b = 1, c;
