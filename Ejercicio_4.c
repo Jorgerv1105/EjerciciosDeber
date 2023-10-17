a5a1f9259b4046fb3a59e8b7bb46c478482dbbee
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-17 01:52:24 -0500

    Inicializacion de variables

---
+++ Ejercicio_4.c
@@ -0,0 +1,7 @@
+//31. Leer números hasta que digiten 0 y determinar a cuánto es igual el promedio de los números terminados en
+//Trejos Buriticá, O. I. (2017). Lógica de programación. Bogotá, Colombia: Ediciones de la U. Recuperado de https://elibro.net/en/ereader/udla/70315?page=229.
+
+#include <stdio.h>
+//Inicializacion de las variables
+int main(void) {
+  int numero, contador = 0, suma = 0, promedio = 0, terminadosEn5 = 0;
