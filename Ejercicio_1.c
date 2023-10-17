c45f01c12b0e4eece16fcada93b54d3cff1befdc
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-17 01:29:35 -0500

    Ingreso de datos

--- Ejercicio_1.c
+++ Ejercicio_1.c
@@ -5,3 +5,16 @@
 int main(void) {
 //Inicializacion de las variables
   int a, b, i;
+//Damos un mensaje inicial para dar un valor a nuestra variable
+  printf("Programa para desplegar los numeros comprendidos entre dos numeros\n");
+  printf("Ingrese el primer numero: ");
+  scanf("%i", &a);
+  printf("Ingrese el segundo numero: ");
+  scanf("%i", &b);
+//Creamos el bucle hasta que se cumpla la condicion mostrada y varios parametros
+  if (a > b) {
+    for (i = --a; i > b; i--) {
+      printf("El resultado es: %i\n", i);
+    }
+  return 0;
+}
