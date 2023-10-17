2f7fd124e142144140ff1df8936d61d603173389
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-17 01:45:21 -0500

    Inicializacion de variables

---
+++ Ejercicio_3.c
@@ -0,0 +1,7 @@
+//22. Leer un número entero y determinar cuántas veces tiene el dígito 1.
+//Trejos Buriticá, O. I. (2017). Lógica de programación. Bogotá, Colombia: Ediciones de la U. Recuperado de https://elibro.net/en/ereader/udla/70315?page=228.
+#include <stdio.h>
+
+int main(void) {
+//Inicializacion de las variables
+  int numero, contador;
