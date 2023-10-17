a8874010608c89ff60d84a5fe15c1ff5c29ce2e4
unknown <jorge.ramos.vasquez@udla.edu.ec>
2023-10-17 01:53:06 -0500

    Bucle

--- Ejercicio_4.c
+++ Ejercicio_4.c
@@ -5,3 +5,8 @@
 //Inicializacion de las variables
 int main(void) {
   int numero, contador = 0, suma = 0, promedio = 0, terminadosEn5 = 0;
+//Damos un mensaje inicial para dar un valor a nuestra variable
+  printf("Ingrese números (ingrese 0 para finalizar):\n");
+//Creamos el bucle hasta que se cumpla la condicion mostrada y varios parametros
+  do {
+    scanf("%i", &numero);
